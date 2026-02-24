04863254ec29bc36af3bf71f1410653502e46362
moin1990 <kemoin@gmail.com>
2026-02-24 09:08:30 +0600

    function part done in js

--- JS/script.js
+++ JS/script.js
@@ -9,7 +9,6 @@ const tabs = document.querySelectorAll(".filters button");
     
     let currentTab = "all";
     
-    /* ------------------ HELPERS ------------------ */
     function getJobs() {
       return document.querySelectorAll(".job-card");
     }
@@ -53,3 +52,45 @@ const tabs = document.querySelectorAll(".filters button");
     
       emptyState.style.display = visible === 0 ? "block" : "none";
     }
+
+    tabs.forEach(tabBtn => {
+        tabBtn.addEventListener("click", () => {
+          tabs.forEach(btn => btn.classList.remove("active"));
+          tabBtn.classList.add("active");
+      
+          currentTab = tabBtn.dataset.tab;
+          filterJobs(currentTab);
+          updateTabCount();
+        });
+      });
+      
+      jobList.addEventListener("click", e => {
+        const card = e.target.closest(".job-card");
+        if (!card) return;
+      
+        const badge = card.querySelector(".badge");
+      
+        if (e.target.classList.contains("interview")) {
+          card.dataset.status = "interview";
+          badge.textContent = "INTERVIEW";
+          badge.className = "badge interview";
+        }
+      
+        if (e.target.classList.contains("rejected")) {
+          card.dataset.status = "rejected";
+          badge.textContent = "REJECTED";
+          badge.className = "badge rejected";
+        }
+      
+        if (e.target.classList.contains("fa-trash-can")) {
+          card.remove();
+        }
+      
+        updateDashboardCounts();
+        filterJobs(currentTab);
+        updateTabCount();
+      });
+            
+      updateDashboardCounts();
+      filterJobs("all");
+      updateTabCount();
